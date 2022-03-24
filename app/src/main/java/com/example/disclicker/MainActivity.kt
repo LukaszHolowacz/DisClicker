61a324f672fd68e88851c7427069d35d379d8f13
Lukas <palczyk69>
2022-03-24 20:44:30 +0100

    Oprawa Graficzna main_activity, dodanie podstawowych zmiennych, działanie przycisku głównego

---
+++ app/src/main/java/com/example/disclicker/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.disclicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val przycisk_podstawowy = findViewById<ImageView>(R.id.jdpoints)
+        val settings_button = findViewById<ImageView>(R.id.settings)
+        val smieszne_ciuszki_button = findViewById<ImageView>(R.id.smieszneciuszki)
+        val score_text = findViewById<TextView>(R.id.score_text)
+        var wynik = 0.toLong()
+
+        var ile_dostajesz = 1
+
+        przycisk_podstawowy.setOnClickListener {
+            wynik += ile_dostajesz
+            score_text.text = wynik.toString()
+        }
+    }
+}
