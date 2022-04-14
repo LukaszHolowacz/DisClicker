09fc93729d7110b74edb288833fd7c75d5a89da4
Lukas <palczyk69>
2022-04-14 15:24:23 +0200

    - Dodanie przycisków nastepna strona i poprzednia w sklepie Śmieszne Ciuszki - Dodanie animacji po wciśnięciu głownego przycisku - Zmiana niektórych kolorów

--- app/src/main/java/com/example/disclicker/MainActivity.kt
+++ app/src/main/java/com/example/disclicker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.disclicker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
@@ -20,11 +21,23 @@ class MainActivity : AppCompatActivity() {
         val smieszne_ciuszki_button = findViewById<ImageView>(R.id.smieszneciuszki)
         val score_text = findViewById<TextView>(R.id.score_text)
 
+        val scaleup = AnimationUtils.loadAnimation(this,R.anim.scale_up)
+        val scaledown = AnimationUtils.loadAnimation(this,R.anim.scale_down)
+        val rotate = AnimationUtils.loadAnimation(this,R.anim.rotate)
+        val fade = AnimationUtils.loadAnimation(this,R.anim.fade)
+
         var ile_dostajesz = 1
 
         przycisk_podstawowy.setOnClickListener {
             wynik += ile_dostajesz
             score_text.text = wynik.toString()
+            if(wynik.mod(69) == 0){
+                przycisk_podstawowy.startAnimation(rotate)
+            }
+            else{
+                przycisk_podstawowy.startAnimation(scaleup)
+                przycisk_podstawowy.startAnimation(scaledown)
+            }
         }
         settings_button.setOnClickListener {
             val intent = Intent(this,settings_activity::class.java)
